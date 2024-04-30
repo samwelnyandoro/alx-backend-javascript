1d1f8f3260e9b0c5ad3acd008d440d75437ff1ca
Sam <samwelnyandoro01@gmail.com>
2024-04-30 14:20:38 +0000

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+
+  if (input) process.stdout.write(`Your name is: ${input}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
