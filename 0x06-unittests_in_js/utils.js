6664f75dafc570408d007d43ff29dff39a5936d9
Sam <samwelnyandoro01@gmail.com>
2024-05-02 07:11:08 +0000

    utils.js

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,13 @@
+class Utils {
+    static calculateNumber(type, a, b) {
+      if (type === 'SUM') return Math.round(a) + Math.round(b);
+      if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
+      if (type === 'DIVIDE' && Math.round(b) === 0) {
+          return 'Error'
+      } else {
+          return Math.round(a) / Math.round(b);
+      }
+    }
+  }
+  
+module.exports = Utils;
